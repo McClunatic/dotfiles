035b46de1a6a3e1ece1465041dce0c4b600e06fa
Brian McClune <brian.mcclune@proton.me>
2024-06-22 10:47:29 -0700

    Add dockerssh function

--- .profile.ps1
+++ .profile.ps1
@@ -16,9 +16,13 @@ function devcontainer {
         devcontainer $SubCommand --workspace-folder $WorkspaceFolder @Passthrough
  }
 
-# Function to format $PWD as MinGW would
-function unixpwd {
-    $PWD -replace '\\','/' -replace 'C:','/c'
+# Function to format path as MinGW would
+function unixpath {
+
+    param (
+        [string] $Path = $PWD
+    )
+    $Path -replace '\\','/' -replace 'C:','/c'
 }
 
 # Function to authorize ed25519 public key in running named container
@@ -32,6 +36,35 @@ function addkey {
     $cmd = "umask 77 && mkdir -p ~/.ssh && cat - >> ~/.ssh/authorized_keys"
     cat ${HOME}\.ssh\id_ed25519.pub | Out-String | `
       docker exec -i $ContainerName bash -c $cmd
+}
+
+# Function to start and ssh to sshd development container
+function dockerssh {
+
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $ContainerName,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $ImageName,
+        [int] $HostPort = 10648,
+        [string] $MountVolume = "codes"
+    )
+
+    $IsMatch = $ImageName -Match ".*/([a-zA-Z0-9\-]+)-sshd:.*"
+    $UserName = $Matches[1]
+
+    # Start the container
+    docker run -d --rm `
+        --name $ContainerName `
+        -p "${HostPort}:10648" `
+        --mount "type=volume,src=codes,dst=/home/${UserName}/codes" `
+        $ImageName
+    # Add key to the container
+    addkey -ContainerName $ContainerName
+    # Delete any matching localhost entries in known_hosts
+    ssh-keygen -q -R [localhost]:${HostPort}
+    # Connect to running container
+    ssh -p $HostPort -o "StrictHostKeyChecking no" -Y ${UserName}@localhost
 }
 
 # Function for managing dotfiles
