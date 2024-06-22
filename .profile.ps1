9185d6470dd73ac368a15fbb88d2af35e04cf949
Brian McClune <brian.mcclune@proton.me>
2024-06-22 05:59:53 -0700

    Add addkey and dotfiles functions

--- .profile.ps1
+++ .profile.ps1
@@ -21,6 +21,30 @@ function unixpwd {
     $PWD -replace '\\','/' -replace 'C:','/c'
 }
 
+# Function to authorize ed25519 public key in running named container
+function addkey {
+
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $ContainerName
+    )
+
+    $cmd = "umask 77 && mkdir -p ~/.ssh && cat - >> ~/.ssh/authorized_keys"
+    cat ${HOME}\.ssh\id_ed25519.pub | Out-String | `
+      docker exec -i $ContainerName bash -c $cmd
+}
+
+# Function for managing dotfiles
+function dotfiles {
+
+    param (
+        [Parameter(ValueFromRemainingArguments=$true)]
+        [string[]] $Passthrough
+    )
+
+    git --git-dir ${HOME}\.dotfiles --work-tree $HOME @Passthrough
+}
+
 # Set up shell to use starship
 if (Get-Command starship -errorAction SilentlyContinue)
 {
