75d4b6522204e2be4281ea3318d483f103458355
Brian McClune <brian.mcclune@proton.me>
2024-06-25 21:27:44 -0400

    Add VS 2022 Developer Powershell to wezterm launcher

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,19 @@ config.enable_scroll_bar = false
 
 if string.find(wezterm.target_triple, 'windows') ~= nil then
   config.default_prog = { 'pwsh.exe' }
+  config.launch_menu = {
+    {
+      label = 'VS 2022 Developer Powershell',
+      args = {
+        'pwsh.exe',
+        '-NoExit',
+        '-Command',
+        '& {Import-Module "C:\\Program Files (x86)\\' ..
+          'Microsoft Visual Studio\\2022\\BuildTools\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"; ' ..
+          'Enter-VsDevShell bcd29cc6 -SkipAutomaticLocation -DevCmdArguments "-arch=arm64 -host_arch=x64"}'
+      }
+    }
+  }
 end
 
 return config
