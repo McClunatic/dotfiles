d23e101962f9ad214cbc61ad725ee685b13351bc
Brian McClune <brian.mcclune@proton.me>
2024-06-26 23:01:38 -0400

    Update bootstrap.sh to resolve Debian errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,27 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Clone dotfiles repo
 if [[ ! -d $HOME/.dotfiles ]]
 then
+    echo 'cloning dotfiles...'
     git clone --bare git@github.com:McClunatic/dotfiles.git $HOME/.dotfiles
 fi
 
 # Checkout the dotfiles
-alias dotfiles='git --git-dir $HOME/.dotfiles --work-tree $HOME'
-dotfiles checkout 2> /dev/null
+echo 'attempting to checkout dotfiles to $HOME...'
+git --git-dir $HOME/.dotfiles --work-tree $HOME checkout 2> /dev/null
 if [[ $? -eq 1 ]]
 then
     # Back up conflicting dotfiles before retrying on error
+    echo 'encountered conflicts, backing up files in $HOME/.dotfiles-backup...'
     mkdir -p $HOME/.dotfiles-backup
-    dotfiles checkout 2>&1 | grep -E '^\s+' | xargs -I{} mv {} $HOME/.dotfiles-backup/{} 
-    dotfiles checkout
+    git --git-dir $HOME/.dotfiles --work-tree $HOME checkout 2>&1 | grep -E '^\s+' | xargs -I{} mv {} $HOME/.dotfiles-backup/{} 
+    echo 'checking out dotfiles to $HOME...'
+    git --git-dir $HOME/.dotfiles --work-tree $HOME checkout
 fi
+echo 'success!'
 
 # Ignore untracked files
-dotfiles config --local status.showUntrackedFiles no
+echo 'disabling dotfiles status display of untracked files...'
+git --git-dir $HOME/.dotfiles --work-tree $HOME config --local status.showUntrackedFiles no
+echo 'complete!'
