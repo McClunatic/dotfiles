cb031213896ac3e1d64e4da060b95927e5f52db4
Brian McClune <brian.mcclune@proton.me>
2024-07-04 07:11:13 -0400

    Set font size explicitly for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 -- Apply configuration changes
 config.color_scheme = 'Tokyo Night'
 config.enable_scroll_bar = false
+config.font_size = 12.0
 
 if string.find(wezterm.target_triple, 'windows') ~= nil then
   config.default_prog = { 'pwsh.exe' }
