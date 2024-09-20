b171c0fe3bd2acdb3133da7db0d6ae0df8b930dc
Brian McClune <brian.mcclune@proton.me>
2024-09-20 19:24:02 +0000

    Move remainder of .profile to .bashrc

---
+++ .bashrc.d/05-basic-env.sh
@@ -0,0 +1,10 @@
+# set the default editor
+if command -v vim &> /dev/null ; then
+    export EDITOR=vim
+fi
+
+# set the default pager
+if command -v less &> /dev/null ; then
+    export PAGER=less 
+fi
+
