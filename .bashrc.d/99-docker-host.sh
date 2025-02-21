2236893132acc48fb5700ff48387d95d1a16c326
Brian McClune <brian.mcclune@proton.me>
2025-02-21 10:32:44 -0500

    Add 99-docker-host.sh for DOCKER_HOST with podman

---
+++ .bashrc.d/99-docker-host.sh
@@ -0,0 +1,5 @@
+# See: https://k3d.io/v5.4.1/usage/advanced/podman/
+# Set DOCKER_HOST to point to rootless podman socket
+if [ -f "$XDG_RUNTIME_DIR/podman/podman.sock" ] ; then
+  export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
+fi
